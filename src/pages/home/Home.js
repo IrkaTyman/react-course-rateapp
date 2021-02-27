e7c634b74339a14d04fb37a14f16d646bafa2d42
Ирина Туманова <51997784+IrkaTyman@users.noreply.github.com>
2021-02-27 18:14:46 +0500

    Add files via upload

---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+import './Home.css'
+import {Exchange} from '../../components/exchange/Exchange'
+
+export const Home = () => {
+
+	return(
+		<Exchange/>	
+		)
+}
